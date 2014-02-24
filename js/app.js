2bb7d670c8d3bbe1fca38875be9da7fc8c1e3dbb
catalinmiron <catalin.miron@ubervu.com>
2014-02-24 10:25:38 +0200

    Fix display issues for wow.js and index.html

--- js/app.js
+++ js/app.js
@@ -10499,6 +10499,20 @@ return jQuery;
   All custom scripts goes here !
 */
 
+// Initiliaze WOW.js
 $(document).ready(function(){
-  console.log('Done !');
+  wow = new WOW(
+    {
+      boxClass:     'wow',      // default
+      animateClass: 'animated', // default
+      offset:       0          // default
+    }
+  );
+  wow.init();
+
+  // Add window height for each section
+  // $('section').each(function(){
+  //   var $window_height = $(window).height();
+  //   $(this).height($window_height);
+  // });
 });
